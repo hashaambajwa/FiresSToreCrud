337088330e787a868bb46f1adc03e5e93b45675e
hashaam2002 <hashaam2002@gmail.com>
2023-11-23 14:46:37 -0500

    create Endpoint Setup

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const admin =  require("firebase-admin");
+const credentials = require("./key.json");
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+})
+
+const db = admin.firestore();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+
+app.post('/create', async (req, res) => {
+    try{   
+        console.log(req.body);
+        const userJson = {
+            email: req.body.email,
+            firstName : req.body.firstName,
+            lastName : req.body.lastName
+        }
+        const response = db.collection("users").doc(userJson.email).set(userJson);
+        res.send(response);
+    }
+    catch (error){
+        res.send(error);
+
+    }
+})
+
+const PORT  = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log('Server is running on PORT: ' + PORT);
+});
+
